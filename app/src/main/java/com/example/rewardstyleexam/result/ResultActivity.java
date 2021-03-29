4f78146f94321e7fcfdc559540efccd317ee3e48
moroboshidan007 <liyinghao0106@gmail.com>
2021-03-29 13:25:56 -0700

    Added result page to show the result

---
+++ app/src/main/java/com/example/rewardstyleexam/result/ResultActivity.java
@@ -0,0 +1,16 @@
+package com.example.rewardstyleexam.result;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.rewardstyleexam.R;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
