a9a9274202a614829d03c3783ead2ca5d3b897cd
moroboshidan007 <liyinghao0106@gmail.com>
2021-03-29 13:10:34 -0700

    Added ui for file name input page

--- app/src/main/java/com/example/rewardstyleexam/file_name_input/controller/MainActivity.java
+++ app/src/main/java/com/example/rewardstyleexam/file_name_input/controller/MainActivity.java
@@ -1,9 +1,16 @@
-package com.example.rewardstyleexam;
+package com.example.rewardstyleexam.file_name_input.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.rewardstyleexam.R;
+
+/**
+ * Main Activity for user to put in the JSON file name.
+ * @author Yinghao Li
+ * @since 2021-03-29
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
